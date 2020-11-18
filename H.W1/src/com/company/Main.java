5f69d187eeb9f586d22f71a27e044179f27ff03b
Рауан <ibragimov020302@gmail.com>
2020-11-18 18:08:27 +0600

    HW

--- H.W1/src/com/company/Main.java
+++ H.W1/src/com/company/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
-        
+        int a = 3;
+        int b = 2;
+        int sum = 0;
+        sum = a + b;
+        System.out.println(sum);
     }
 }
