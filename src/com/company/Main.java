69b003b131a8bee6d008a4e3a48246cf5f0de83c
Рауан <ibragimov020302@gmail.com>
2021-04-08 21:50:28 +0600

    HW54

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -18,17 +19,20 @@ public class Main {
         hashmap.put(8,"Bayern");
         hashmap.put(9,"Madrid");
         hashmap.put(10,"Arsenal");
-        for (Map.Entry<Integer,String> item : hashmap.entrySet()){
-            if (item.getKey() > 5){
-                System.out.println(item.getValue());
-            }
-        }
-        for (Map.Entry<Integer,String> item : hashmap.entrySet()){
-            if (item.getKey() % 3 == 0){
-                System.out.print(item.getValue() + ", ");
-            }
-        }
+
+        Optional<String> values1 = hashmap.keySet().stream().filter(x -> x > 5).map(hashmap::get).reduce((value1,combined) -> value1 + "," + combined);
+        System.out.println(values1.orElse(null) + " ");
+
+        System.out.println("*******************************");
+
+        Optional<String> values2 = hashmap.keySet().stream().filter(x -> x % 3 == 0).map(hashmap::get).reduce((value1, combined) -> value1 + "," + combined);
+        System.out.print(values2.orElse(null) + " ");
         System.out.println();
+
+        System.out.println("*******************************");
+//        Stream<Integer> values3  = hashmap.keySet().stream().filter(x -> x > 5).map(hashmap::get).;
+//        System.out.println(values3 + " ");
+
         for (Map.Entry<Integer,String> item : hashmap.entrySet()){
             if (item.getKey() > 5){
                 System.out.println(item.getKey()*item.getKey());
@@ -45,6 +49,7 @@ public class Main {
         ///B.///
         ////////
 
+        System.out.println("*******************************");
         Integer[] arr = {10,12,55,12,42,25,35,64,57,78,98};
         Arrays.stream(arr).filter(x -> x % 2 == 0).forEach(x -> System.out.println(x));
         List<Integer> list = Arrays.stream(arr).filter(x -> x % 2 != 0).map(x -> x + 10).collect(Collectors.toList());
